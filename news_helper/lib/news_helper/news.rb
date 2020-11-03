a54a8fc8c959ad83495b025dc1f9cbd344eac95c
Christian Simon <csimon1127@gmail.com>
2020-11-03 22:28:13 +0000

    Updated the README.md, LICENSE, and added core class methods for lib/news_helper/news.rb.

--- news_helper/lib/news_helper/news.rb
+++ news_helper/lib/news_helper/news.rb
@@ -1,3 +1,20 @@
 class NewsHelper::News
-    
+  attr_accessor :headline, :publish_time, :url
+  
+  def self.get_news
+    news = [
+    	self.scrape_tech_sites,
+    	self.scrape_health_sites,
+    	self.scrape_politics
+  	]
+  	binding.pry
+  	news
+  end
+  
+  
+  
+  
+  
+  
+  
 end
