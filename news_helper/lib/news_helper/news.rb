e4ee83f679658fb48929338da50d9d505626e05d
Christian Simon <csimon1127@gmail.com>
2020-11-04 02:58:29 +0000

    Added self.scrape_political_sites to lib/news_helper/news.rb, which scrapes the political sites for their articles. Works similarly to self.scrape_tech_sites.

--- news_helper/lib/news_helper/news.rb
+++ news_helper/lib/news_helper/news.rb
@@ -5,7 +5,7 @@ class NewsHelper::News
     news = [
     	self.scrape_tech_sites,
     	self.scrape_health_sites,
-    	self.scrape_politics
+    	self.scrape_political_sites
   	]
   	binding.pry
   	news
@@ -86,5 +86,43 @@ class NewsHelper::News
   	}
   	tech_news
   end  
+  
+  def self.scrape_political_sites 
+  	tech_news = [] 
+  	bus = Nokogiri::HTML(open("https://www.businessinsider.com/s?q=politics"))
+  	cnn = Nokogiri::HTML(open("https://www.cnn.com/politics"))
+  	fox = Nokogiri::HTML(open("https://www.foxnews.com/politics/"))
+  	
+  	bus.css(".river-item .featured-post").each_with_index(0) {|section, i|
+  		if i < 5
+  			article = self.new
+  			article.headline = bus.css("a .tout-title-link").text.strip
+  			article.publish_date = bus.css(".tout-timestamp .headline-regular .js-date-format .js-rendered").text.strip
+  			article.url = "https://www.businessinsider.com" + bus.css("a .tout-title-link").attribute("href").value.strip
+  			tech_news << article
+  		end
+  	}
+  
+  	cnn.css(".cnn-search__result .cnn-search_result--article").each_with_index(0) {|section, i|
+  		if i < 5
+  			article = self.new
+  			article.headline = cnn.css("cnn-search__result-headline").text.strip
+  			article.publish_date = bus.css(".cnn-search__result-publish-date").text.strip 
+  			article.url = cnn.css("cnn-search__result-headline").attribute("href").value.strip
+  			tech_news << article
+  		end
+  	}
+  
+  	fox.css("article .article").each_with_index(0) {|section, i|
+  		if i < 5
+  			article = self.new
+  			article.headline = fox.css("a .tout-title-link").text.strip
+  			article.publish_date = bus.css(".time").text.strip
+  			article.url = "https://www.foxnews.com" + fox.css(".title").attribute("href").value.strip
+  			tech_news << article
+  		end
+  	}
+  	tech_news
+  end
   
 end
