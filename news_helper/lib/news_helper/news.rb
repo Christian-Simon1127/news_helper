bedec0ac194f395a719c73680b46b101fee63279
Christian Simon <csimon1127@gmail.com>
2020-11-04 01:39:13 +0000

    Added part of self.scrape_tech_sites in lib/news_helper/news.rb, which scrapes the tech sites for their articles.

--- news_helper/lib/news_helper/news.rb
+++ news_helper/lib/news_helper/news.rb
@@ -11,7 +11,21 @@ class NewsHelper::News
   	news
   end
   
-  
+  def self.scrape_tech_sites 
+  	tech_news = [] #In the order of business insider, cnn, fox news 
+  	bus = Nokogiri::HTML(open("https://www.businessinsider.com/sai"))
+  	cnn = Nokogiri::HTML(open("https://www.cnn.com/search?q=tech&size=5&sort=newest"))
+  	fox = Nokogiri::HTML(open("https://www.foxnews.com/tech/"))
+  	
+  	bus.css(".river-item .featured-post").each_with_index(0) {|section, i|
+  		if i < 5
+  			article = self.new 
+  			article.headline = bus.css("a .tout-title-link").text.strip
+  			article.publish_date = bus.css(".tout-timestamp .headline-regular .js-date-format .js-rendered").text.strip
+  			article.url = "https://www.businessinsider.com" + bus.css("a .tout-title-link").attribute("href").value.strip
+  			tech_news << article
+  		end
+  	} 
   
   
   
