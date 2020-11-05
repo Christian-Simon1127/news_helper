79821de0b202ba7f9c9b5781f215d34ee353734f
Christian Simon <csimon1127@gmail.com>
2020-11-05 20:01:43 +0000

    Fixed some errors in logic in lib/news_helper/news.rb.

--- lib/news_helper/news.rb
+++ lib/news_helper/news.rb
@@ -1,11 +1,11 @@
 class NewsHelper::News
-  attr_accessor :headline, :publish_time, :url
+  attr_accessor :headline, :publish_date, :url
   
   def self.get_news
     news = [
-    	self.scrape_tech_sites,
-    	self.scrape_health_sites,
-    	self.scrape_political_sites
+    	self.scrape_tech_sites#,
+    	#self.scrape_health_sites,
+    	#self.scrape_political_sites
   	]
   	binding.pry
   	news
@@ -17,112 +17,94 @@ class NewsHelper::News
   	cnn = Nokogiri::HTML(URI.open("https://www.cnn.com/search?q=tech&size=5&sort=newest"))
   	fox = Nokogiri::HTML(URI.open("https://www.foxnews.com/tech/"))
   	
-  	bus.css(".river-item .featured-post").each_with_index {|section, i|
-  		if i < 5
+  	bus.css("#l-content").each_with_index {|section|
   			article = self.new 
   			article.headline = bus.css("a .tout-title-link").text.strip
   			article.publish_date = bus.css(".tout-timestamp .headline-regular .js-date-format .js-rendered").text.strip
-  			article.url = "https://www.businessinsider.com" + bus.css("a .tout-title-link").attribute("href").value.strip
+  			article.url = "https://www.businessinsider.com" + bus.css("a .tout-title-link").attribute("href").strip
   			tech_news << article
-  		end
   	} 
   
-  	cnn.css(".cnn-search__result .cnn-search_result--article").each_with_index {|section, i|
-  		if i < 5
+  	cnn.css(".cnn-search__result .cnn-search_result--article").each {|section|
   			article = self.new
   			article.headline = cnn.css("cnn-search__result-headline").text.strip
   			article.publish_date = bus.css(".cnn-search__result-publish-date").text.strip 
   			article.url = "https://www.cnn.com" + cnn.css("cnn-search__result-headline").attribute("href").value.strip
   			tech_news << article
-  		end
   	}
   
-  	fox.css("article .article").each_with_index {|section, i|
-  		if i < 5
+  	fox.css("article .article").each {|section|
   			article = self.new
   			article.headline = fox.css(".title").text.strip
   			article.publish_date = bus.css(".time").text.strip
   			article.url = "https://www.foxnews.com" + fox.css(".title").attribute("href").value.strip
   			tech_news << article
-  		end
   	}
   	tech_news
   end
   
   def self.scrape_health_sites 
-  	tech_news = []
+  	health_news = []
   	bus = Nokogiri::HTML(URI.open("https://www.businessinsider.com/healthcare")) 
   	cnn = Nokogiri::HTML(URI.open("https://www.cnn.com/health"))
   	fox = Nokogiri::HTML(URI.open("https://www.foxnews.com/health"))
   	
-  	bus.css(".river-item .featured-post").each_with_index {|section, i|
-  		if i < 5
-  			article = self.new
+  	bus.css("#l-content").each_with_index {|section|
+  			article = self.new 
   			article.headline = bus.css("a .tout-title-link").text.strip
   			article.publish_date = bus.css(".tout-timestamp .headline-regular .js-date-format .js-rendered").text.strip
-  			article.url = "https://www.businessinsider.com" + bus.css("a .tout-title-link").attribute("href").value.strip
+  			article.url = "https://www.businessinsider.com" + bus.css("a .tout-title-link").attribute("href").strip
   			tech_news << article
-  		end
-  	}
+  	} 
   
-  	cnn.css(".cnn-search__result .cnn-search_result--article").each_with_index {|section, i|
-  		if i < 5
+  	cnn.css(".cnn-search__result .cnn-search_result--article").each_with_index {|section|
   			article = self.new
   			article.headline = cnn.css("cnn-search__result-headline").text.strip
   			article.publish_date = bus.css(".cnn-search__result-publish-date").text.strip 
   			article.url = cnn.css("cnn-search__result-headline").attribute("href").value.strip
-  			tech_news << article
-  		end
+  			health_news << article
   	}
   
-  	fox.css("article .article").each_with_index {|section, i|
-  		if i < 5
+  	fox.css("article .article").each_with_index {|section|
   			article = self.new
   			article.headline = fox.css(".title").text.strip
   			article.publish_date = bus.css(".time").text.strip
   			article.url = "https://www.foxnews.com" + fox.css(".title").attribute("href").value.strip
-  			tech_news << article
-  		end
+  			health_news << article
   	}
-  	tech_news
+  	health_news
   end  
   
   def self.scrape_political_sites 
-  	tech_news = [] 
+  	poli_news = [] 
   	bus = Nokogiri::HTML(URI.open("https://www.businessinsider.com/s?q=politics"))
   	cnn = Nokogiri::HTML(URI.open("https://www.cnn.com/search?q=politics"))
   	fox = Nokogiri::HTML(URI.open("https://www.foxnews.com/politics/"))
   	
-  	bus.css(".river-item .featured-post").each_with_index {|section, i|
-  		if i < 5
-  			article = self.new
+  	bus.css("#l-content").each_with_index {|section|
+  			article = self.new 
   			article.headline = bus.css("a .tout-title-link").text.strip
   			article.publish_date = bus.css(".tout-timestamp .headline-regular .js-date-format .js-rendered").text.strip
-  			article.url = "https://www.businessinsider.com" + bus.css("a .tout-title-link").attribute("href").value.strip
+  			article.url = "https://www.businessinsider.com" + bus.css("a .tout-title-link").attribute("href").strip
   			tech_news << article
-  		end
-  	}
+  	} 
   
   	cnn.css(".cnn-search__result .cnn-search_result--article").each_with_index {|section, i|
-  		if i < 5
   			article = self.new
   			article.headline = cnn.css("cnn-search__result-headline").text.strip
   			article.publish_date = bus.css(".cnn-search__result-publish-date").text.strip 
   			article.url = cnn.css("cnn-search__result-headline").attribute("href").value.strip
-  			tech_news << article
-  		end
+  			poli_news << article
   	}
   
   	fox.css("article .article").each_with_index {|section, i|
-  		if i < 5
   			article = self.new
   			article.headline = fox.css(".title").text.strip
   			article.publish_date = bus.css(".time").text.strip
   			article.url = "https://www.foxnews.com" + fox.css(".title").attribute("href").value.strip
-  			tech_news << article
-  		end
+  			poli_news << article
   	}
-  	tech_news
+  	poli_news
   end
   
 end
