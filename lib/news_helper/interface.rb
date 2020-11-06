2379a9fd0527f269a076f92e3892676b33620966
Christian Simon <csimon1127@gmail.com>
2020-11-06 04:43:48 +0000

    Changed the NewsHelper::CLI start method, and added display_news, list_options and farewell into lib/news_helper/interface.rb.

--- lib/news_helper/interface.rb
+++ lib/news_helper/interface.rb
@@ -1,10 +1,22 @@
 class NewsHelper::CLI
   
   def start
-    @news = NewsHelper::News.get_news
+    display_news
+    list_options
+    farewell
   end 
   
+  def display_news
+    
+  end 
   
+  def list_options
+    
+  end 
+  
+  def farewell
+    
+  end 
   
   
 end
